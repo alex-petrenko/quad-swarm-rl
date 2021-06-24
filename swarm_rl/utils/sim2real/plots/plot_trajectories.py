b83316f483f5bfe83c696cab95e85717334cd633
SumeetBatra <sscifi@gmail.com>
2021-06-24 10:23:56 -0700

    minor plotting changes

--- swarm_rl/utils/sim2real/plots/plot_trajectories.py
+++ swarm_rl/utils/sim2real/plots/plot_trajectories.py
@@ -9,27 +9,32 @@ from mpl_toolkits.mplot3d import Axes3D
 if __name__ == '__main__':
     cf_ids = [7, 10, 11, 12]
     dfs = []
-    file = 'static_goal/static_goal_data.txt'
+    file = 'swap_goals/swap_goals_data.txt'
     cfs_data = []
-    # for cfid in cf_ids:
-    #     file = f'swap_goals/cf{cfid}.txt'
-    #     df = pd.read_csv(file)
-    #     dfs.append(df)
-    #     print(type(df['field.values1']))
-    #     print(list(df['field.values1']))
+    plot_together = False
     df = pd.read_csv(file)
+    fig = plt.figure()
     for cfid in cf_ids:
         cf_data = df.loc[df['field.transforms0.child_frame_id'] == f'cf{cfid}'][['field.transforms0.transform.translation.x',
                                                                  'field.transforms0.transform.translation.y',
                                                                  'field.transforms0.transform.translation.z']].values
         cfs_data.append(cf_data)
 
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    for cf_data in cfs_data:
-        ax.plot(cf_data[:, 0], cf_data[:, 1], cf_data[:, 2])
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    ax.set_zlabel('z')
+    if not plot_together:
+        ax = fig.add_subplot(111, projection='3d')
+        for cf_data in cfs_data:
+            ax.plot(cf_data[:, 0], cf_data[:, 1], cf_data[:, 2])
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.set_zlabel('z')
+    else:
+        for i in range(len(cf_ids)):
+            cfid = cf_ids[i]
+            cf_data = cfs_data[i]
+            ax = fig.add_subplot(2, 2, i+1, projection='3d')
+            ax.plot(cf_data[:, 0], cf_data[:, 1], cf_data[:, 2])
+            ax.set_xlabel('x')
+            ax.set_ylabel('y')
+            ax.set_zlabel('z')
+            ax.set_title(f'cf{cfid}')
     plt.show()
